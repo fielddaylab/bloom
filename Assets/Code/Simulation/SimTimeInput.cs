1739f75f5fdb25bc5a0876c7b84d7bb0c8ad55ab
Amelia <tpmoser@wisc.edu>
2023-09-26 17:12:34 -0500

    script pause refinements
    - Added LeafMembers for pausing the game
    - Event pauses now "outrank" user pauses

--- Assets/Code/Simulation/SimTimeInput.cs
+++ Assets/Code/Simulation/SimTimeInput.cs
@@ -1,4 +1,6 @@
 using FieldDay.Systems;
+using Leaf.Runtime;
+using UnityEngine;
 using Zavala.Input;
 
 namespace Zavala.Sim {
@@ -11,14 +13,45 @@ namespace Zavala.Sim {
             }
 
             if (m_StateB.ButtonPressed(InputButton.Pause)) {
-                if ((m_StateA.Paused & SimPauseFlags.User) != 0) {
-                    m_StateA.Paused &= ~SimPauseFlags.User;
-                    m_StateA.PauseOverlay.gameObject.SetActive(false);
-                } else {
-                    m_StateA.Paused |= SimPauseFlags.User;
-                    m_StateA.PauseOverlay.gameObject.SetActive(true);
-                }
+                TogglePause(SimPauseFlags.User);
             }
         }
+
+        public static void SetPaused(bool pause, SimPauseFlags flag) {
+            SimTimeState time = FieldDay.Game.SharedState.Get<SimTimeState>();
+            if (pause) {
+                time.Paused |= flag;
+                time.PauseOverlay.gameObject.SetActive(true);
+            } else {
+                if (time.Paused > flag) {
+                    Debug.Log("[SimTimeInput] User attempting to unpause an event pause, ignoring...");
+                    // player attempts to unpause an event pause
+                    return;
+                }
+                time.Paused = 0;
+                time.PauseOverlay.gameObject.SetActive(false);
+            }
+        }
+
+        public static void TogglePause(SimPauseFlags flag) {
+            SimTimeState time = FieldDay.Game.SharedState.Get<SimTimeState>();
+            SetPaused(time.Paused == 0, flag);
+        }
+
+        [LeafMember("TogglePause")]
+        public static void TogglePauseEvent() {
+            TogglePause(SimPauseFlags.Event);
+        }
+
+        [LeafMember("Pause")]
+        public static void PauseEvent() {
+            SetPaused(true, SimPauseFlags.Event);
+        }
+
+        [LeafMember("Unpause")]
+        public static void UnpauseEvent() {
+            SetPaused(false, SimPauseFlags.Event);
+        }
+
     }
 }
