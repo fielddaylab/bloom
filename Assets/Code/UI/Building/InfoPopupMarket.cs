ea75a039ea2f188a0a079e6fc015d96dff1dc736
levimhuillet <levimhuillet@gmail.com>
2023-12-19 13:42:05 -0600

    fix for seller total profit calculation bug

--- Assets/Code/UI/Building/InfoPopupMarket.cs
+++ Assets/Code/UI/Building/InfoPopupMarket.cs
@@ -258,12 +258,20 @@ namespace Zavala.UI.Info {
             col.ShippingCol.gameObject.SetActive(true);
             SetMoneyText(col.ShippingCol.Number, (-shippingPrice).ToStringLookup());
 
-            int import = info.TaxRevenue.Import;
+            int import;
+            if (info.Supplier.Position.RegionIndex == info.Requester.Position.RegionIndex)
+            {
+                import = 0;
+            }
+            else
+            {
+                import = config.UserAdjustmentsPerRegion[info.Requester.Position.RegionIndex].ImportTax[info.Resource];
+            }
             col.ImportTaxCol.gameObject.SetActive(false);
             if (import == 0) { col.ImportTaxCol.Number.SetText(EmptyEntry); }
             else { SetMoneyText(col.ImportTaxCol.Number, (-import).ToStringLookup()); }
 
-            int salesTax = info.TaxRevenue.Sales;
+            int salesTax = config.UserAdjustmentsPerRegion[info.Requester.Position.RegionIndex].PurchaseTax[info.Resource];
             col.SalesTaxCol.gameObject.SetActive(false);
             if (salesTax == 0) { col.SalesTaxCol.Number.SetText(EmptyEntry); }
             else { SetMoneyText(col.SalesTaxCol.Number, (-salesTax).ToStringLookup()); }
@@ -376,164 +384,7 @@ namespace Zavala.UI.Info {
 
         #region Load Rows
 
-        static public void LoadLocationIntoRow(InfoPopupLocationRow row, OccupiesTile location, OccupiesTile referenceLocation)
-        {
-            LocationDescription desc = location.GetComponent<LocationDescription>();
-
-            row.NameLabel.SetText(Loc.Find(desc.TitleLabel));
-            row.Icon.sprite = desc.Icon;
-
-            if (location.IsExternal)
-            {
-                row.RegionLabel.gameObject.SetActive(true);
-                row.RegionLabel.SetText(Loc.Find("region.external.name"));
-                row.NameLabel.rectTransform.SetAnchorPos(row.RegionLabel.rectTransform.sizeDelta.y / 2, Axis.Y);
-            }
-            else if (referenceLocation != null && location.RegionIndex == referenceLocation.RegionIndex)
-            {
-                row.RegionLabel.gameObject.SetActive(false);
-                row.NameLabel.rectTransform.SetAnchorPos(0, Axis.Y);
-            }
-            else
-            {
-                row.RegionLabel.gameObject.SetActive(true);
-                row.RegionLabel.SetText(Loc.Find(RegionUtility.GetNameLong(location.RegionIndex)));
-                row.NameLabel.rectTransform.SetAnchorPos(row.RegionLabel.rectTransform.sizeDelta.y / 2, Axis.Y);
-            }
-        }
-
-        static public void LoadCostsIntoRow(InfoPopupLocationRow row, MarketQueryResultInfo info, MarketConfig config, bool isSecondary)
-        {
-            row.PriceGroup.SetActive(true);
-
-            int marketIndex = MarketUtility.ResourceIdToMarketIndex(info.Resource);
-            int basePrice = info.Supplier.PriceNegotiator.SellPriceBlock[marketIndex]; // config.DefaultPurchasePerRegion[info.Supplier.Position.RegionIndex].Buy[info.Resource];
-
-            row.BasePriceRow.gameObject.SetActive(true);
-            row.BasePriceRow.Number.SetText(basePrice.ToStringLookup());
-
-            int shippingPrice = info.ShippingCost;
-            row.ShippingRow.gameObject.SetActive(shippingPrice > 0);
-            if (shippingPrice > 0)
-            {
-                row.ShippingRow.Number.SetText(shippingPrice.ToStringLookup());
-            }
-
-            int import = info.TaxRevenue.Import;
-            row.ImportTaxRow.gameObject.SetActive(import > 0);
-            row.SubsidyRow.gameObject.SetActive(import < 0);
-            if (import > 0)
-            {
-                row.ImportTaxRow.Number.SetText((-import).ToStringLookup());
-            }
-            else if (import < 0)
-            {
-                row.SubsidyRow.Number.SetText((-import).ToStringLookup());
-            }
-
-            int salesTax = info.TaxRevenue.Sales;
-            row.SalesTaxRow.gameObject.SetActive(salesTax > 0);
-            if (salesTax > 0)
-            {
-                row.SalesTaxRow.Number.SetText((-salesTax).ToStringLookup());
-            }
-
-            int penalties = info.TaxRevenue.Penalties;
-            row.PenaltyRow.gameObject.SetActive(penalties > 0);
-            if (penalties > 0)
-            {
-                row.PenaltyRow.Number.SetText((-penalties).ToStringLookup());
-            }
-
-            int totalCost = basePrice + shippingPrice + import + salesTax + penalties;
-            row.TotalProfitRow.gameObject.SetActive(false);
-            row.TotalPriceRow.gameObject.SetActive(true);
-            row.TotalPriceRow.Number.SetText(totalCost.ToStringLookup());
-
-            if (isSecondary)
-            {
-                row.TotalPriceRow.Background.color = NegativeColorBG;
-                row.TotalPriceRow.Number.color = NegativeColor;
-            }
-            else
-            {
-                row.TotalPriceRow.Background.color = PositiveColorBG;
-                row.TotalPriceRow.Number.color = PositiveColor;
-            }
-
-            //row.PriceLayout.ForceRebuild(true);
-        }
-
-        static public void LoadProfitIntoRow(InfoPopupLocationRow row, MarketQueryResultInfo info, MarketConfig config, bool isSecondary)
-        {
-            row.PriceGroup.SetActive(true);
-
-            int basePrice;
-            if (info.Requester.IsLocalOption)
-            {
-                basePrice = 0;
-            }
-            else
-            {
-                int marketIndex = MarketUtility.ResourceIdToMarketIndex(info.Resource);
-                basePrice = info.Supplier.PriceNegotiator.SellPriceBlock[marketIndex]; // config.DefaultPurchasePerRegion[info.Supplier.Position.RegionIndex].Buy[info.Resource];
-            }
-            row.BasePriceRow.gameObject.SetActive(basePrice > 0);
-            row.BasePriceRow.Number.SetText(basePrice.ToStringLookup());
-
-            int shippingPrice = info.ShippingCost;
-            row.ShippingRow.gameObject.SetActive(shippingPrice > 0);
-            if (shippingPrice > 0)
-            {
-                row.ShippingRow.Number.SetText((-shippingPrice).ToStringLookup());
-            }
-
-            int import = info.TaxRevenue.Import;
-            row.ImportTaxRow.gameObject.SetActive(import > 0);
-            row.SubsidyRow.gameObject.SetActive(import < 0);
-            if (import > 0)
-            {
-                row.ImportTaxRow.Number.SetText((-import).ToStringLookup());
-            }
-            else if (import < 0)
-            {
-                row.SubsidyRow.Number.SetText((-import).ToStringLookup());
-            }
-
-            // Commenting out for now - sales tax is applied to the buyer
-            /*            
-            int salesTax = info.TaxRevenue.Sales;
-            row.SalesTaxRow.gameObject.SetActive(salesTax > 0);
-            if (salesTax > 0) {
-                row.SalesTaxRow.Number.SetText((-salesTax).ToStringLookup());
-            }
-            */
-
-            int penalties = info.TaxRevenue.Penalties;
-            row.PenaltyRow.gameObject.SetActive(penalties > 0);
-            if (penalties > 0)
-            {
-                row.PenaltyRow.Number.SetText((-penalties).ToStringLookup());
-            }
-
-            int totalCost = info.Profit;
-            row.TotalProfitRow.gameObject.SetActive(true);
-            row.TotalPriceRow.gameObject.SetActive(false);
-            row.TotalProfitRow.Number.SetText(totalCost.ToStringLookup());
-
-            if (isSecondary)
-            {
-                row.TotalProfitRow.Background.color = NegativeColorBG;
-                row.TotalProfitRow.Number.color = NegativeColor;
-            }
-            else
-            {
-                row.TotalProfitRow.Background.color = PositiveColorBG;
-                row.TotalProfitRow.Number.color = PositiveColor;
-            }
-
-            //row.PriceLayout.ForceRebuild(true);
-        }
+        
 
         static public void ClearPricesAtRow(InfoPopupLocationRow row)
         {
