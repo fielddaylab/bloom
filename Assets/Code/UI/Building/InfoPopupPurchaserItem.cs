9881b61fd1ae46469dd4b46564dde6b1633a0cdb
levimhuillet <levimhuillet@gmail.com>
2023-12-18 18:53:10 -0600

    City popup info, additional grain/cafo UI polish

---
+++ Assets/Code/UI/Building/InfoPopupPurchaserItem.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zavala.UI.Info
+{
+    public class InfoPopupPurchaserItem : MonoBehaviour
+    {
+        public Image Icon;
+        public TMP_Text Description;
+    }
+}
