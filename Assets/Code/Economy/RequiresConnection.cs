8f6ddf491cc439950d6bb97d5819d2fc71c4d326
levimhuillet <levimhuillet@gmail.com>
2023-12-20 13:34:50 -0600

    disconnected from road popup; new destroy confirm button

---
+++ Assets/Code/Economy/RequiresConnection.cs
@@ -0,0 +1,12 @@
+using FieldDay.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zavala.Economy
+{
+    public class RequiresConnection : BatchedComponent
+    {
+
+    }
+}
