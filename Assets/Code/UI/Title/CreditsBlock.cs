0331f484c3333d878a257799a3a696f7edab84e0
levimhuillet <levimhuillet@gmail.com>
2024-01-25 16:53:25 -0600

    first pass on credits

---
+++ Assets/Code/UI/Title/CreditsBlock.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CreditsBlock : MonoBehaviour
+{
+    public TMP_Text Header;
+    public TMP_Text Names;
+}
--- Assets/Code/UI/Title/UICredits.cs
+++ Assets/Code/UI/Title/UICredits.cs
@@ -7,11 +7,23 @@ using UnityEngine.UI;
 using BeauRoutine.Extensions;
 using Zavala.Cards;
 using TMPro;
+using BeauUtil;
+using System.Text;
+using System;
+using BeauUtil.Debugger;
 
 namespace Zavala.UI
 {
+    public struct CreditsBlockData
+    {
+        public string Header;
+        public string Names;
+    }
+
     public class UICredits : MonoBehaviour
     {
+        private static string CREDITS_DELIM = "::";
+
         private static float TRANSITION_X = 1060;
         private static float TRANSITION_TIME = 0.5f;
 
@@ -23,7 +35,22 @@ namespace Zavala.UI
         [Space(5)]
         [Header("Text")]
         [SerializeField] private TextAsset m_CreditsTextAsset;
-        [SerializeField] private TMP_Text m_CreditsText;
+        [SerializeField] private RectTransform m_TextContainer;
+        [SerializeField] private LayoutGroup m_TextLayout;
+
+        [SerializeField] private GameObject m_CreditsBlockPrefab;
+
+        [Space(5)]
+        [Header("Settings")]
+        [SerializeField] private float m_ScrollTime;
+        [SerializeField] private float m_ScrollTimeBuffer;
+
+        private bool m_Scrolling;
+        private float m_ScrollSpeed;
+        private float m_ScrollTimer;
+
+        private List<CreditsBlockData> m_CreditsBlockDatas;
+        private List<CreditsBlock> m_CreditsBlocks;
 
         #endregion // Inspector
 
@@ -36,6 +63,29 @@ namespace Zavala.UI
             m_ReturnButton.onClick.AddListener(HandleReturnClicked);
 
             ParseCredits();
+            CreditsBlocksToText();
+
+            // Calculate scroll speed based on scroll time and distance to travel
+            Assert.True(m_ScrollTime > 0);
+            if (m_CreditsBlocks.Count > 0) {
+                m_ScrollSpeed = (m_CreditsBlocks[0].transform.position.y - m_CreditsBlocks[m_CreditsBlocks.Count - 1].transform.position.y + m_TextContainer.rect.size.y) / (m_ScrollTime);
+            }
+
+            ResetLayout();
+        }
+
+        private void Update()
+        {
+            if (m_Scrolling && m_ScrollTimer > -m_ScrollTimeBuffer)
+            {
+                m_TextContainer.transform.position = new Vector3(
+                    m_TextContainer.transform.position.x,
+                    m_TextContainer.transform.position.y + m_ScrollSpeed * Time.deltaTime,
+                    m_TextContainer.transform.position.z
+                    );
+
+                m_ScrollTimer -= Time.deltaTime;
+            }
         }
 
         #endregion // Unity Callbacks
@@ -62,7 +112,9 @@ namespace Zavala.UI
 
         private IEnumerator EnterRoutine()
         {
+            m_ScrollTimer = m_ScrollTime;
             yield return m_Rect.AnchorPosTo(0, TRANSITION_TIME, Axis.X).Ease(Curve.CubeOut);
+            m_Scrolling = true;
         }
 
         private IEnumerator ExitRoutine()
@@ -76,18 +128,70 @@ namespace Zavala.UI
 
         private void ResetLayout()
         {
-            
+            // Text position
+            m_TextContainer.transform.localPosition = new Vector3(
+                m_TextContainer.transform.localPosition.x,
+                0 - m_TextContainer.rect.size.y,
+                m_TextContainer.transform.localPosition.z
+                );
+
+            m_Scrolling = false;
+
+            // Image Sequence
+
         }
 
         #region Credits Parsing
 
         private void ParseCredits()
         {
-            List<string> creditsBlocks = TextIO.TextAssetToList(m_CreditsTextAsset, "::");
+            List<string> blocksToParse = TextIO.TextAssetToList(m_CreditsTextAsset, CREDITS_DELIM);
+            m_CreditsBlockDatas = new List<CreditsBlockData>();
 
+            for (int i = 0; i < blocksToParse.Count; i++)
+            {
+                // skip first space
+                if (i == 0) { continue; }
 
+                m_CreditsBlockDatas.Add(ParseCreditsBlock(blocksToParse[i]));
+            }
+        }
 
-            m_CreditsText.SetText(creditsBlocks[0]);
+        private CreditsBlockData ParseCreditsBlock(string blocktoParse)
+        {
+            CreditsBlockData newBlock = new CreditsBlockData();
+
+            // Trim leading delim
+            int startIdx = blocktoParse.IndexOf(CREDITS_DELIM) + CREDITS_DELIM.Length;
+
+            // Split at first newline
+            int newlineIdx;
+            
+            int nIndex = blocktoParse.IndexOf("\n");
+            int rIndex = blocktoParse.IndexOf("\r");
+
+            newlineIdx = Math.Max(nIndex, rIndex);
+            
+            newBlock.Header = blocktoParse.Substring(startIdx, newlineIdx - startIdx).Trim();
+            newBlock.Names = blocktoParse.Substring(newlineIdx + 1, blocktoParse.Length - newlineIdx - 1);
+
+            return newBlock;
+        }
+
+        private void CreditsBlocksToText()
+        {
+            m_CreditsBlocks = new List<CreditsBlock>();
+
+            for (int i = 0; i < m_CreditsBlockDatas.Count; i++)
+            {
+                var newTextBlock = Instantiate(m_CreditsBlockPrefab, m_TextContainer.transform).GetComponent<CreditsBlock>();
+                newTextBlock.Header.SetText(m_CreditsBlockDatas[i].Header);
+                newTextBlock.Names.SetText(m_CreditsBlockDatas[i].Names);
+
+                m_CreditsBlocks.Add(newTextBlock);
+            }
+
+            m_TextLayout.ForceRebuild();
         }
 
         #endregion // Credits Parsing
