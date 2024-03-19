920adf3d901cdd274e44581b90926eb077f17f87
levimhuillet <levimhuillet@gmail.com>
2024-03-19 16:44:19 -0500

    added concept art to credits

--- Assets/Code/UI/Title/UICredits.cs
+++ Assets/Code/UI/Title/UICredits.cs
@@ -12,6 +12,8 @@ using System.Text;
 using System;
 using BeauUtil.Debugger;
 using Zavala.Data;
+using EasyAssetStreaming;
+using UnityEditor;
 
 namespace Zavala.UI
 {
@@ -32,6 +34,8 @@ namespace Zavala.UI
 
         [SerializeField] private Button m_ReturnButton;
         [SerializeField] private RectTransform m_Rect;
+        [SerializeField] private StreamingUGUITexture m_ArtImg;
+        [SerializeField] private string[] m_ArtPaths;
 
         [Space(5)]
         [Header("Text")]
@@ -50,12 +54,21 @@ namespace Zavala.UI
         private float m_ScrollSpeed;
         private float m_ScrollTimer;
 
+        private float m_RecycleImgTime;
+        private float m_RecycleImgTimer;
+        private int m_NumConceptImgs;
+        private int m_CurrArtIndex;
+        private int m_ArtDirLength = "Assets/StreamingAssets/".Length;
+
+        private Routine m_ConceptRoutine;
+
         private List<CreditsBlockData> m_CreditsBlockDatas;
         private List<CreditsBlock> m_CreditsBlocks;
 
         #endregion // Inspector
 
         private Routine m_TransitionRoutine;
+        private bool m_Transitioning;
 
         #region Unity Callbacks
 
@@ -66,11 +79,15 @@ namespace Zavala.UI
             ParseCredits();
             CreditsBlocksToText();
 
+            ConceptImageSetup();
+
             // Calculate scroll speed based on scroll time and distance to travel
             Assert.True(m_ScrollTime > 0);
             if (m_CreditsBlocks.Count > 0) {
                 m_ScrollSpeed = (m_CreditsBlocks[0].transform.position.y - m_CreditsBlocks[m_CreditsBlocks.Count - 1].transform.position.y + m_TextContainer.rect.size.y) / (m_ScrollTime);
             }
+
+            m_RecycleImgTime = m_RecycleImgTimer = m_ScrollTime / m_NumConceptImgs;
 
             ResetLayout();
         }
@@ -86,6 +103,12 @@ namespace Zavala.UI
                     );
 
                 m_ScrollTimer -= Time.deltaTime;
+                m_RecycleImgTimer -= Time.deltaTime;
+
+                if (m_RecycleImgTimer <= 0)
+                {
+                    m_ConceptRoutine.Replace(AdvanceConceptArtRoutine());
+                }
             }
         }
 
@@ -96,6 +119,7 @@ namespace Zavala.UI
         private void HandleReturnClicked()
         {
             ZavalaGame.Events.Dispatch(GameEvents.MainMenuInteraction, MenuInteractionType.CreditsExited);
+            if (m_Transitioning) { return; }
             m_TransitionRoutine.Replace(this, ExitRoutine());
         }
 
@@ -105,6 +129,7 @@ namespace Zavala.UI
 
         public void OpenPanel()
         {
+            if (m_Transitioning) { return; }
             m_TransitionRoutine.Replace(this, EnterRoutine());
         }
 
@@ -114,16 +139,58 @@ namespace Zavala.UI
 
         private IEnumerator EnterRoutine()
         {
+            m_Transitioning = true;
             m_ScrollTimer = m_ScrollTime;
+            if (m_CurrArtIndex != 0)
+            {
+                m_CurrArtIndex = 0;
+                m_ArtImg.Path = m_ArtPaths[m_CurrArtIndex];
+                m_ArtImg.Preload();
+                while (m_ArtImg.IsLoading())
+                {
+                    yield return null;
+                }
+            }
             yield return m_Rect.AnchorPosTo(0, TRANSITION_TIME, Axis.X).Ease(Curve.CubeOut);
             m_Scrolling = true;
+            m_Transitioning = false;
         }
 
         private IEnumerator ExitRoutine()
         {
+            m_Transitioning = true;
             yield return m_Rect.AnchorPosTo(TRANSITION_X, TRANSITION_TIME, Axis.X).Ease(Curve.CubeOut);
 
             ResetLayout();
+
+            if (m_CurrArtIndex != 0)
+            {
+                m_CurrArtIndex = 0;
+                m_ArtImg.Path = m_ArtPaths[m_CurrArtIndex];
+                m_ArtImg.Preload();
+                while (m_ArtImg.IsLoading())
+                {
+                    yield return null;
+                }
+            }
+
+            m_Transitioning = false;
+        }
+
+        private IEnumerator AdvanceConceptArtRoutine()
+        {
+            m_RecycleImgTimer = m_RecycleImgTime;
+
+            if (m_CurrArtIndex == m_NumConceptImgs) { yield break; }
+
+            m_ArtImg.Path = m_ArtPaths[m_CurrArtIndex];
+            m_ArtImg.Preload();
+            while (m_ArtImg.IsLoading())
+            {
+                yield return null;
+            }
+
+            m_CurrArtIndex++;
         }
 
         #endregion // Routines
@@ -141,6 +208,21 @@ namespace Zavala.UI
 
             // Image Sequence
 
+        }
+
+        private void ConceptImageSetup()
+        {
+            string[] assetGuids = AssetDatabase.FindAssets("*", new string[] { "Assets/StreamingAssets/credits" });
+            m_NumConceptImgs = assetGuids.Length;
+
+            m_ArtPaths = new string[m_NumConceptImgs];
+            int imgIndex = 0;
+            foreach (var guid in assetGuids)
+            {
+                string fullPath = AssetDatabase.GUIDToAssetPath(guid);
+                m_ArtPaths[imgIndex] = fullPath.Substring(m_ArtDirLength);
+                imgIndex++;
+            }
         }
 
         #region Credits Parsing
